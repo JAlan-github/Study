b98413af9a0c4de098f8e75d8567144a0017f626
Alan Ji <jalanspirit@gmail.com>
2013-06-22 03:01:02 +0000

    Add ReverseSingleList

--- Study/src/algorithm/list/Node.java
+++ Study/src/algorithm/list/Node.java
@@ -4,4 +4,46 @@ public class Node {
 	public int data;
 	public Node prev;
 	public Node next;
+
+	public Node() {
+
+	}
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public Node(int data, Node prev, Node next) {
+		this.data = data;
+		this.prev = prev;
+		this.next = next;
+	}
+	
+	public static Node generateSingleLinkedList(int[] data) {
+		if(data.length == 0 || data == null)
+			return null;
+		else if(data.length == 1)
+			return new Node(data[0]);
+		else {
+			Node header = new Node(data[0]);
+			Node curr = header;
+			for(int i=1; i<data.length; i++) {
+				curr.next = new Node(data[i]);
+				curr = curr.next;
+			}
+			
+			return header;
+		}
+	}
+	
+	public void print() {
+		Node temp = this;
+		
+		while(temp != null) {
+			System.out.print(temp.data);
+			if(temp.next != null)
+				System.out.print(" -> ");
+			temp = temp.next;
+		}
+	}
 }
---
+++ Study/src/algorithm/list/ReverseSingleList.java
@@ -0,0 +1,42 @@
+package algorithm.list;
+
+public class ReverseSingleList {
+	public static Node header;
+	
+	public static void main(String[] args) {
+		Node node = Node.generateSingleLinkedList(new int[]{1,2,3,4,5,6,7});
+		node.print();
+		System.out.println("\nReverse List:");
+//		reverseSingleList(node);
+//		header.print();
+		reverseSingleListNoRecursive(node).print();
+	}
+	
+	public static Node reverseSingleList(Node node) {
+		if(node.next == null)
+			return header = node;
+		
+		Node newHeader = reverseSingleList(node.next);
+		newHeader.next = node;
+		node.next = null;
+		
+		return node;
+	}
+	
+	public static Node reverseSingleListNoRecursive(Node node) {
+		Node prev = null;
+		Node curr = node;
+		Node next = null;
+		
+		while(curr != null) {
+			next = curr.next;
+			
+			curr.next = prev;
+			prev = curr;
+			
+			curr = next;
+		}
+		
+		return prev;
+	}
+}
